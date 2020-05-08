cf1f7154b854707b05f463e83f5b0fe744f519b4
sunkadshreyas <sunkad.shreyas@gmail.com>
2020-05-08 11:05:00 +0530

    Added HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Contains the logic of the application
+# It will request information from the model and pass it to a template.
+
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
