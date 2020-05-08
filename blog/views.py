7e0f25cb0348d581886edd86e7a928dffab229c3
sunkadshreyas <sunkad.shreyas@gmail.com>
2020-05-08 11:42:59 +0530

    Modeified template to display post from database

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 # Contains the logic of the application
 # It will request information from the model and pass it to a template.
+# Take a post from Models and pass it to template(HTML)
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    # Published blog posts sorted by published date
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
+
