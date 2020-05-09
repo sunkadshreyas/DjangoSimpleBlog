15fef53fd3dc605a25afd4919b683f48fdce2c7e
sunkadshreyas <sunkad.shreyas@gmail.com>
2020-05-09 15:57:32 +0530

    fixing the edit error

--- blog/views.py
+++ blog/views.py
@@ -18,17 +18,17 @@ def post_detail(request,pk):
     return render(request,'blog/post_detail.html',{'post':post})
 
 def post_new(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -42,4 +42,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})                          
+    return render(request, 'blog/post_edit.html', {'form': form})                      
